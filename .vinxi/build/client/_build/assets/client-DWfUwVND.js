5dce25e1c15eb9bee82e8fbba2f269daeb0edeac
newsve <newsve@proton.me>
2025-01-31 17:00:13 +0000

    fix gitignore again

--- .vinxi/build/client/_build/assets/client-DWfUwVND.js
+++
@@ -1,1 +0,0 @@
-import{c as f}from"./client-BE1L4nLD.js";export{f as default};
--- .vinxi/build/client/_build/assets/hello-32X9uJco.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./client-BE1L4nLD.js";const e=function(){return o.jsx("div",{children:'Hello "/hello"!'})};export{e as component};
